b82fd55d45c00db953c8168f1f6fcb91b42c83c1
Abhishek Rathore <abhirathore@565gmail.com>
2021-05-22 15:19:12 +0530

    Added Remdesivir, Tocilizumab, Oxygen, Ration and News endpoints

--- controllers/getTweets.js
+++ controllers/getTweets.js
@@ -21,6 +21,26 @@ const icuQuery =
 const foodQuery =
 "verified Indore ((food delivery) OR food) -\"not verified\" -\"unverified\" -\"needed\" -\"need\" -\"needs\" -\"required\" -\"require\" -\"requires\" -\"requirement\" -\"requirements\"";
 
+// prettier-ignore
+const rationQuery =
+"verified Indore ((food delivery) OR food) -\"not verified\" -\"unverified\" -\"needed\" -\"need\" -\"needs\" -\"required\" -\"require\" -\"requires\" -\"requirement\" -\"requirements\"";
+
+// prettier-ignore
+const remdesivirQuery =
+"verified Indore ((Remdesivir delivery) OR Remdesivir) -\"not verified\" -\"unverified\" -\"needed\" -\"need\" -\"needs\" -\"required\" -\"require\" -\"requires\" -\"requirement\" -\"requirements\"";
+
+// prettier-ignore
+const tocilizumabQuery =
+"verified Indore ((Tocilizumab delivery) OR Tocilizumab) -\"not verified\" -\"unverified\" -\"needed\" -\"need\" -\"needs\" -\"required\" -\"require\" -\"requires\" -\"requirement\" -\"requirements\"";
+
+// prettier-ignore
+const oxygenQuery =
+"verified Indore ((oxygen cylinder) OR oxygen) -\"not verified\" -\"unverified\" -\"needed\" -\"need\" -\"needs\" -\"required\" -\"require\" -\"requires\" -\"requirement\" -\"requirements\"";
+
+// prettier-ignore
+const newsQuery =
+"verified Indore ((covid news) OR covid) -\"not verified\" -\"unverified\" -\"needed\" -\"need\" -\"needs\" -\"required\" -\"require\" -\"requires\" -\"requirement\" -\"requirements\"";
+
 const getBeds = (req, res) => {
 	const url = reqUrl + bedQuery;
 	axios
@@ -138,6 +158,207 @@ const getFood = (req, res) => {
 		});
 };
 
+const getRation = (req, res) => {
+	const url = reqUrl + rationQuery;
+	axios
+		.get(url, {
+			headers: {
+				Authorization: "Bearer " + process.env.BEARER_TOKEN,
+			},
+		})
+		.then((response) => {
+			let tweets = response.data.data;
+			console.log(response);
+			let newTweets = [];
+			tweets.forEach(async (tweet) => {
+				const tweetId = tweet.id;
+				const tweetInfo = await getTweetInfo(tweetId);
+				const userInfo = await getUsername(tweetInfo.author_id);
+
+				const newTweet = {
+					id: tweetId,
+					createdAt: tweetInfo.created_at,
+					authorId: userInfo.id,
+					authorName: userInfo.name,
+					authorUsername: userInfo.username,
+					text: tweet.text,
+				};
+
+				newTweets.push(newTweet);
+
+				if (newTweets.length == tweets.length) {
+					res.json(newTweets);
+				}
+			});
+
+			// res.json(response.data.data);
+		})
+		.catch((error) => {
+			res.send("Error.");
+		});
+};
+
+const getRemdesivir = (req, res) => {
+	const url = reqUrl + remdesivirQuery;
+	axios
+		.get(url, {
+			headers: {
+				Authorization: "Bearer " + process.env.BEARER_TOKEN,
+			},
+		})
+		.then((response) => {
+			let tweets = response.data.data;
+			let newTweets = [];
+			tweets.forEach(async (tweet) => {
+				const tweetId = tweet.id;
+				const tweetInfo = await getTweetInfo(tweetId);
+				const userInfo = await getUsername(tweetInfo.author_id);
+
+				const newTweet = {
+					id: tweetId,
+					createdAt: tweetInfo.created_at,
+					authorId: userInfo.id,
+					authorName: userInfo.name,
+					authorUsername: userInfo.username,
+					text: tweet.text,
+				};
+
+				newTweets.push(newTweet);
+
+				if (newTweets.length == tweets.length) {
+					res.json(newTweets);
+				}
+			});
+
+			// res.json(response.data.data);
+		})
+		.catch((error) => {
+			res.send("Error.");
+		});
+};
+
+const getTocilizumab = (req, res) => {
+	const url = reqUrl + tocilizumabQuery;
+	axios
+		.get(url, {
+			headers: {
+				Authorization: "Bearer " + process.env.BEARER_TOKEN,
+			},
+		})
+		.then((response) => {
+			let tweets = response.data.data;
+			let newTweets = [];
+			tweets.forEach(async (tweet) => {
+				const tweetId = tweet.id;
+				const tweetInfo = await getTweetInfo(tweetId);
+				const userInfo = await getUsername(tweetInfo.author_id);
+
+				const newTweet = {
+					id: tweetId,
+					createdAt: tweetInfo.created_at,
+					authorId: userInfo.id,
+					authorName: userInfo.name,
+					authorUsername: userInfo.username,
+					text: tweet.text,
+				};
+
+				newTweets.push(newTweet);
+
+				if (newTweets.length == tweets.length) {
+					res.json(newTweets);
+				}
+			});
+
+			// res.json(response.data.data);
+		})
+		.catch((error) => {
+			res.send("Error.");
+		});
+};
+
+const getOxygen = (req, res) => {
+	const url = reqUrl + oxygenQuery;
+	axios
+		.get(url, {
+			headers: {
+				Authorization: "Bearer " + process.env.BEARER_TOKEN,
+			},
+		})
+		.then((response) => {
+			let tweets = response.data.data;
+			let newTweets = [];
+			tweets.forEach(async (tweet) => {
+				const tweetId = tweet.id;
+				const tweetInfo = await getTweetInfo(tweetId);
+				const userInfo = await getUsername(tweetInfo.author_id);
+
+				const newTweet = {
+					id: tweetId,
+					createdAt: tweetInfo.created_at,
+					authorId: userInfo.id,
+					authorName: userInfo.name,
+					authorUsername: userInfo.username,
+					text: tweet.text,
+				};
+
+				newTweets.push(newTweet);
+
+				if (newTweets.length == tweets.length) {
+					res.json(newTweets);
+				}
+			});
+
+			// res.json(response.data.data);
+		})
+		.catch((error) => {
+			res.send("Error.");
+		});
+};
+
+const getNews = (req, res) => {
+	const url = reqUrl + newsQuery;
+	axios
+		.get(url, {
+			headers: {
+				Authorization: "Bearer " + process.env.BEARER_TOKEN,
+			},
+		})
+		.then((response) => {
+			let tweets = response.data.data;
+			let newTweets = [];
+			tweets.forEach(async (tweet) => {
+				const tweetId = tweet.id;
+				const tweetInfo = await getTweetInfo(tweetId);
+				const userInfo = await getUsername(tweetInfo.author_id);
+
+				const newTweet = {
+					id: tweetId,
+					createdAt: tweetInfo.created_at,
+					authorId: userInfo.id,
+					authorName: userInfo.name,
+					authorUsername: userInfo.username,
+					text: tweet.text,
+				};
+
+				newTweets.push(newTweet);
+
+				if (newTweets.length == tweets.length) {
+					res.json(newTweets);
+				}
+			});
+
+			// res.json(response.data.data);
+		})
+		.catch((error) => {
+			res.send("Error.");
+		});
+};
+
 module.exports.getBeds = getBeds;
-module.exports.getFood = getFood;
 module.exports.getICU = getICU;
+module.exports.getFood = getFood;
+module.exports.getRation = getRation;
+module.exports.getRemdesivir = getRemdesivir;
+module.exports.getTocilizumab = getTocilizumab;
+module.exports.getOxygen = getOxygen;
+module.exports.getNews = getNews;
--- routes/getTweets.js
+++ routes/getTweets.js
@@ -1,9 +1,23 @@
 const router = require("express").Router();
 
-const { getBeds, getICU, getFood } = require("./../controllers/getTweets");
+const {
+	getBeds,
+	getICU,
+	getFood,
+	getRation,
+	getRemdesivir,
+	getTocilizumab,
+	getOxygen,
+	getNews,
+} = require("./../controllers/getTweets");
 
 router.get("/beds", getBeds);
 router.get("/icu", getICU);
 router.get("/food", getFood);
+router.get("/ration", getRation);
+router.get("/remdesivir", getRemdesivir);
+router.get("/tocilizumab", getTocilizumab);
+router.get("/oxygen", getOxygen);
+router.get("/news", getNews);
 
 module.exports = router;
