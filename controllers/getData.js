ec38cd5603f1ca4e647cac883cac71443dae1bf9
Abhishek Rathore <abhirathore@565gmail.com>
2021-05-25 11:20:24 +0530

    Added different routes for different types of data

--- controllers/getData.js
+++ controllers/getData.js
@@ -1,6 +1,6 @@
 const Detail = require("./../models/Detail");
 
-const getData = async (req, res) => {
+const getAllData = async (req, res) => {
 	try {
 		const data = await Detail.find().sort({ updatedAt: -1 }).limit(100);
 		return res.status(200).json(data);
@@ -9,4 +9,69 @@ const getData = async (req, res) => {
 	}
 };
 
-module.exports = getData;
+const getBedData = async (req, res) => {
+	const type = "beds";
+	try {
+		const data = await Detail.find({ type: type })
+			.sort({ updatedAt: -1 })
+			.limit(100);
+		return res.status(200).json(data);
+	} catch (error) {
+		return res.json(error);
+	}
+};
+
+const getFoodData = async (req, res) => {
+	const type = "food";
+	try {
+		const data = await Detail.find({ type: type })
+			.sort({ updatedAt: -1 })
+			.limit(100);
+		return res.status(200).json(data);
+	} catch (error) {
+		return res.json(error);
+	}
+};
+
+const getRemdesivirData = async (req, res) => {
+	const type = "remdesivir";
+	try {
+		const data = await Detail.find({ type: type })
+			.sort({ updatedAt: -1 })
+			.limit(100);
+		return res.status(200).json(data);
+	} catch (error) {
+		return res.json(error);
+	}
+};
+
+const getInjectionData = async (req, res) => {
+	const type = "injection";
+	try {
+		const data = await Detail.find({ type: type })
+			.sort({ updatedAt: -1 })
+			.limit(100);
+		return res.status(200).json(data);
+	} catch (error) {
+		return res.json(error);
+	}
+};
+
+const getOxygenData = async (req, res) => {
+	const type = "oxygen";
+	try {
+		const data = await Detail.find({ type: type })
+			.sort({ updatedAt: -1 })
+			.limit(100);
+		return res.status(200).json(data);
+	} catch (error) {
+		return res.json(error);
+	}
+};
+
+module.exports.getAllData = getAllData;
+module.exports.getBedData = getBedData;
+module.exports.getFoodData = getFoodData;
+module.exports.getRemdesivirData = getRemdesivirData;
+module.exports.getInjectionData = getInjectionData;
+module.exports.getOxygenData = getOxygenData;
--- routes/addData.js
+++ routes/addData.js
@@ -1,10 +1,22 @@
 const router = require("express").Router();
 
 const addData = require("./../controllers/addData");
-const getData = require("./../controllers/getData");
+const {
+	getAllData,
+	getBedData,
+	getFoodData,
+	getRemdesivirData,
+	getInjectionData,
+	getOxygenData,
+} = require("./../controllers/getData");
 
 router.post("/add", addData);
 
-router.get("/get", getData);
+router.get("/get/all", getAllData);
+router.get("/get/beds", getBedData);
+router.get("/get/food", getFoodData);
+router.get("/get/remdesivir", getRemdesivirData);
+router.get("/get/injection", getInjectionData);
+router.get("/get/oxygen", getOxygenData);
 
 module.exports = router;
